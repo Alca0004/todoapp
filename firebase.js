8e9450ee03c3296d7a15ffad0be40f4148fd808c
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-03-03 15:08:57 -0500

    Setting up
    Firebase
    Cleaning template

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDV45v4p8vzl5DlyB-LA6Df9G98l3Pllqk',
+  authDomain: 'todo-app-d073a.firebaseapp.com',
+  projectId: 'todo-app-d073a',
+  storageBucket: 'todo-app-d073a.appspot.com',
+  messagingSenderId: '418636347251',
+  appId: '1:418636347251:web:d734ed036dc15b0e3fb706',
+  measurementId: 'G-95MXXWCMTT',
+};
