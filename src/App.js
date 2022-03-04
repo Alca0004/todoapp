f086808f02d7a980ac786641588d78cc67beb885
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-03-04 13:23:57 -0500

    Mark2
    Adding material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, FormControl, InputLabel, Input } from '@mui/material';
 
 function App() {
+  const [todos, setTodos] = useState(['']);
+
+  const [input, setInput] = useState('');
+  // this fires off the event after click the button
+  // console.log('🚀', input);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    // console.log('It works');
+    setTodos([...todos, input]);
+    setInput(''); // clears input
+  };
+
   return (
     <div className="App">
-      <h1>To Do App</h1>
+      <h1>TODOLISTAPP</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel>Write a Todo✅</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+
+        <Button disabled={!input} onClick={addTodo} variant="contained">
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
